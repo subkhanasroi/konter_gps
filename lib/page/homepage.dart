ee9e003274cebce859f8ea1a4db1121090de46d0
subkhanasroi <asroisubkhan01@gmail.com>
2022-11-08 16:35:46 +0700

    [add] icon apps

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return ListView(
+      children: [],
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:konter_gps/main.dart';
+import 'package:konter_gps/konter_gps.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const KonterGps());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
